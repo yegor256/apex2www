447a5f2dca67c289591f705beaaa7dbc96bb41b9
Yegor Bugayenko <yegor256@gmail.com>
2024-02-15 17:05:58 +0300

    #1 skeleton

---
+++ README.md
@@ -0,0 +1,16 @@
+[![docker](https://github.com/yegor256/apex2www/actions/workflows/docker.yml/badge.svg)](https://github.com/yegor256/apex2www/actions/workflows/docker.yml)
+[![Docker Cloud Automated build](https://img.shields.io/docker/cloud/automated/yegor256/apex2www)](https://hub.docker.com/r/yegor256/apex2www)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/yegor256/total/apex2www/master/LICENSE.txt)
+
+[This](https://hub.docker.com/r/yegor256/apex2www) [Docker](https://www.docker.com/)
+image helps you start your own apex/root to www redirecting HTTP server.
+
+When Docker is [installed](https://docs.docker.com/install/), run this:
+
+```bash
+$ docker run --name apex2www --detach --restart=always --publish 80:80 yegor256/apex2www
+```
+
+Now you can point `A` DNS record of your domain to the IP address of the
+server where the Docker container is running. Once an HTTP request arrives,
+it will automatically be redirected to the `www.` hostname.
---
+++ test/test_apex2www.js
@@ -0,0 +1,43 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const assert = require('assert');
+const runSync = require('./helpers').runSync;
+const version = require('../src/version');
+
+describe('apex2www', function() {
+  it('prints its own version', function(done) {
+    const stdout = runSync(['--version']);
+    assert.equal(version.what + '\n', stdout);
+    done();
+  });
+
+  it('prints help screen', function(done) {
+    const stdout = runSync(['--help']);
+    assert(stdout.includes('Usage: apex2www'));
+    assert(stdout.includes(version.what));
+    assert(stdout.includes(version.when));
+    done();
+  });
+});
